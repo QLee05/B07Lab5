1b9acc6af1ce453882c3c3941433002f70e1660b
nikhilkaku <nikhilkaku11@gmail.com>
2024-10-26 22:30:05 -0400

    feat: added unit tests for Rectangle class

---
+++ RectangleTests.java
@@ -0,0 +1,33 @@
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+import java.util.List;
+import java.beans.Transient;
+import java.util.ArrayList;
+
+class RectangleTesting {
+
+    @Test
+    void testPerimeter(){
+        Point p = new Point(0,0);
+        Rectangle R = new Rectangle(p, 10, 10);
+
+        assertEquals(R.perimeter(), 16);
+    }
+
+    @Test
+    void testArea(){
+        Point p = new Point(0,0);
+        Rectangle R = new Rectangle(p, 10, 10);
+
+        assertEquals(R.area(), 100);
+    }
+
+    @Test
+    void testIsSquare(){
+        Point p = new Point(0,0);
+        Rectangle R = new Rectangle(p, 10, 10);
+
+        assertFalse(R.isSquare());
+    } 
+}
